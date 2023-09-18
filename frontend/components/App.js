9839725817fc8f0e76d8a0d49b174e4a99bd7837
melissaannee <melissaevangelista52@gmail.com>
2023-09-18 00:47:44 -0400

    completed Character component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,23 +1,72 @@
-import React from 'react'
-import axios from 'axios'
-import Character from './Character'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Character from "./Character";
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlPlanets = "http://localhost:9009/api/planets";
+const urlPeople = "http://localhost:9009/api/people";
 
 function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
-  return (
-    <div>
-      <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
-    </div>
-  )
+	const [people, setPeople] = useState([]);
+	const [planets, setPlanets] = useState([]);
+	const [data, setData] = useState([]);
+	console.log("DATA", data);
+
+	const fetch = async (url, setter) => {
+		await axios
+			.get(url)
+			.then((data) => {
+				setter(data.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	const mergedData = async () => {
+		let newData = people.map((value) => {
+			let newObj = {
+				id: value.id,
+				name: value.name,
+			};
+			let homePlanet = planets.find((planet) => {
+				return planet.id === value.homeworld;
+			});
+			newObj.homeworldName = homePlanet.name;
+			return newObj;
+		});
+
+		setData(newData);
+	};
+
+	useEffect(() => {
+		fetch(urlPeople, setPeople);
+		fetch(urlPlanets, setPlanets);
+	}, []);
+
+	useEffect(() => {
+		if (people.length && planets.length) {
+			mergedData();
+		}
+	}, [people, planets]);
+
+	console.log(data);
+
+	return (
+		<div>
+			<h2>Star Wars Characters</h2>
+			<p>
+				See the README of the project for instructions on completing this
+				challenge
+			</p>
+			{/* ❗ Map over the data in state, rendering a Character at each iteration */}
+			{data.map((value) => {
+				return <Character key={value.id} data={value} />;
+			})}
+		</div>
+	);
 }
 
-export default App
+export default App;
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
-if (typeof module !== 'undefined' && module.exports) module.exports = App
+if (typeof module !== "undefined" && module.exports) module.exports = App;
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Character() { // ❗ Add the props
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  // ❗ Create a "toggle" click handler to show or remove the homeworld
-  return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
-    </div>
-  )
+function Character({ data }) {
+	// ❗ Create a state to hold whether the homeworld is rendering or not
+	const [showHomeworld, setShowHomeworld] = useState(false);
+	// ❗ Create a "toggle" click handler to show or remove the homeworld
+	const toggle = () => {
+		setShowHomeworld(!showHomeworld);
+	};
+
+	return (
+		<div className="character-card" onClick={toggle}>
+			<h3 className="character-name">{data.name}</h3>
+			{showHomeworld ? (
+				<p>
+					Planet: <span className="character-planet">{data.homeworldName}</span>
+				</p>
+			) : null}
+			{/* Use the same markup with the same attributes as in the mock */}
+		</div>
+	);
 }
 
-export default Character
+export default Character;
